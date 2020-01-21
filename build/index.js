dd7d527dcafd55a7e4d75eca32fef4a9a32e241b
KirillIvano <ivannadez@yandex.ru>
2020-01-21 22:57:00 +0300

    рефактор

--- build/index.js
+++ build/index.js
@@ -1,21 +1,21 @@
 // delete concrete user
 const deleteUser = () => 
     chrome.tabs.executeScript(null, {
-        file: 'tabs/deleteUser.js',
+        file: 'tabs/eraseUser.js',
     });
 
 const MESSAGES_URL = 'https://vk.com/im';
 
-const checkUrlAndDelete = (url, timeout) => MESSAGES_URL === url && setTimeout(deleteUser, timeout);
+const checkUrlAndEraseUser = (url, timeout) => MESSAGES_URL === url && setTimeout(deleteUser, timeout);
 
 // handle page change
-chrome.history.onVisited.addListener(({url}) => checkUrlAndDelete(url, 500));
+chrome.history.onVisited.addListener(({url}) => checkUrlAndEraseUser(url, 500));
 
 // handle tab change
 chrome.tabs.onActivated.addListener(({tabId}) => {
     chrome.tabs.get(
         tabId,
-        ({url}) => checkUrlAndDelete(url, 0)
+        ({url}) => checkUrlAndEraseUser(url, 0)
     );
 });
 
